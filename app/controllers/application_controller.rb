71dfa15e57ae762a6cda38250756d04e1d91e6d3
Elemigrante <gasdom@yandex.ru>
2019-05-06 10:37:09 +0300

    add helper method for photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   helper_method :current_user_can_edit?
+  helper_method :user_can_add_photos?
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update) do |u|
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
   def current_user_can_edit?(model)
     user_signed_in? &&
       (model.user == current_user || (model.try(:event).present? && model.event.user == current_user))
+  end
+
+  def user_can_add_photos?(event)
+    user_signed_in? && event.visitors.include?(current_user)
   end
 end
